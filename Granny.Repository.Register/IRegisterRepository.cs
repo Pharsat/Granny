c09741a9e1090c6191693cfe4450a4534b920b48
Cristian Felipe Gallego Ramírez <pharsat@hotmail.com>
2019-10-25 13:03:49 -0500

    Se agrega nuevo proyecto repositorio para el API de registro

---
+++ Granny.Repository.Register/IRegisterRepository.cs
@@ -0,0 +1,12 @@
+﻿using Granny.DataModel;
+using System;
+using System.Threading.Tasks;
+
+namespace Granny.Repository.Register
+{
+    public interface IRegisterRepository
+    {
+        Task InsertProduct(Product product);
+        Task<int> InsertPrice(Price price);
+    }
+}
