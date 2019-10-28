a5c6b32799effd1a74ed790bb52fce2c58022657
Cristian Felipe Gallego Ramírez <pharsat@hotmail.com>
2019-10-28 09:21:05 -0500

    se elimina la referencia a modelo que no estaba siendo usada.

--- Granny.Api.Query/Controllers/V1/ProductController.cs
+++ Granny.Api.Query/Controllers/V1/ProductController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using Granny.Api.Query.Model;
 using Granny.Repository.Query;
 using Granny.Util.Validators;
 using Microsoft.AspNetCore.Http;
