288fa1f8f0208148d9c9124289e6b336dff818b2
Cristian Felipe Gallego Ramírez <pharsat@hotmail.com>
2019-10-28 09:19:21 -0500

    Se cambian los metodos de consulta por funciones get.

--- Granny.Api.Query/Controllers/V1/ProductController.cs
+++ Granny.Api.Query/Controllers/V1/ProductController.cs
@@ -24,20 +24,25 @@ namespace Granny.Api.Query.Controllers.V1
             return Ok(await repository.GetBestProductPrice(pluCode));
         }
 
-        // POST: api/Product
-        [HttpPost]
-        public async Task<IActionResult> GetNextProductPrices([FromBody] GetNextPricesRequestModel value)
+        // GET: api/Product/5/2000
+        [HttpGet("{pluCode}/{price}", Name = "GetNextProductPrices")]
+        public async Task<IActionResult> GetNextProductPrices(
+            [Required]
+            [StringLength(14)]
+            [RegularExpression("^[0-9]*$")] string pluCode, 
+            [Required]
+            [MinValue(typeof(decimal), "0")] decimal price)
         {
             if (!ModelState.IsValid) return BadRequest();
-            return Ok(await repository.GetNextProductPrices(value.PluCode, value.Price));
+            return Ok(await repository.GetNextProductPrices(pluCode, price));
         }
 
-        // POST: api/Product
-        [HttpPost]
-        public async Task<IActionResult> GetPricesByLocation([FromBody] GetPricesByLocationRequestModel value)
+        // GET: api/Product/Exito las vegas
+        [HttpGet("{location}", Name = "GetPricesByLocation")]
+        public async Task<IActionResult> GetPricesByLocation([Required] string location)
         {
             if (!ModelState.IsValid) return BadRequest();
-            return Ok(await repository.GetPricesByLocation(value.Location));
+            return Ok(await repository.GetPricesByLocation(location));
         }
     }
 }
